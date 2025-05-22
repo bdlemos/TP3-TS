aa626918facbafb98f43846c85e981652d0f28d3
bdlemos <bdutralemos@gmail.com>
2025-05-22 16:04:47 +0100

    Salvando nome do usuarios aos logs

--- logger.py
+++ logger.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-def log_action(action, user, path, status):
+def log_action(action, level,path, status):
+    load_dotenv(override=True)  # Load environment variables
+    user = os.getenv("USER", "default_user")
     with open("audit.log", "a") as f:
-        f.write(f"{datetime.now()} | {user} | {action} | {path} | {status}\n")
+        f.write(f"{datetime.now()} | {user} - {level} | {action} | {path} | {status}\n")
